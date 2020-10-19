da3c2c3e7e7f03f1fef4ea8eba44aee1f1d66e04
James McDougall <jameswiliammcdougall@gmail.com>
2020-10-19 11:48:40 -0500

    Added assets to project

---
+++ DesignCodeCourse/DesignCodeCourse/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  DesignCodeCourse
+//
+//  Created by Lambda_School_loaner_226 on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
